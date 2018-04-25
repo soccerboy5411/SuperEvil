2ad858c0e9e6950871c594eccc25b5b9e94fdea0
soccerboy5411 <38662224+soccerboy5411@users.noreply.github.com>
2018-04-25 08:12:36 -0500

    Create tcpServer.py

---
+++ tcpServer.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip,bind_port))
+
+server.listen(5)
+
+print "[*] Received: %s" % request
+
+#send back a packet
+client_socket.send("ACK!")
+
+client_socket.close()
+
+whilte True:
+  client,addr = server.accept()
+  print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+  
+  #spin up out client thread to handle incoming data
+  client_handler = threading.Thread(target=handle_client,args=(client,))
+  client_handler.start()
